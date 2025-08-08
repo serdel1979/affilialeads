4b6f8d0860f76d0987e280fc1e4967b3ddba0b9c
sergio <sdlbsso@gmail.com>
2025-08-08 10:53:37 -0300

    validaciones

--- mail/send-email.php
+++ mail/send-email.php
@@ -2,7 +2,19 @@
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 
+    $env_path = __DIR__ . '/../.env'; 
+    
+    if (file_exists($env_path)) {
+        $lines = file($env_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (strpos(trim($line), '#') === 0) continue;
+            list($name, $value) = explode('=', $line, 2);
+            $_ENV[$name] = $value;
+            putenv("$name=$value");
+        }
+    }
     $recaptcha_secret = getenv('RECAPTCHA_SECRET');
+    
 
 
 
